b0a7f7f114ca308f69f4bbbae75ca4241e2596d0
folujam <olusway@gmail.com>
2024-07-24 12:28:08 +0100

    test 4 #1

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""MRU Caching Module"""
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """inherits from base caching lol duh!
+    -its a caching system
+    -it implements a put and get method"""
+    def __init__(self):
+        """initializes this cache and base cache
+        -orders the dict(cache_data) so as to pop pricisely"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """adds item to cache
+        -if key is empty nothing is done
+        -key in data assigns item
+        -if memory filled old item is popped for new item"""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                # popped_key = list(self.cache_data.keys())[1] only returns val
+                popped_key, _ = self.cache_data.popitem(last=False)
+                print("DISCARD: {}".format(popped_key))
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """checks if key value exist
+        - returns key value"""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
