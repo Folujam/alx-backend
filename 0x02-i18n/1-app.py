0c86110919656ed907064fdd08f03b5b329f0357
folujam <olusway@gmail.com>
2024-07-31 20:47:47 +0100

    t1 done, test

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""a flask app Module"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+# Create a Flask app instance
+app = Flask(__name__)
+
+
+# Create config class definition with attributes
+class Config:
+    """config class that inits ffg attribs"""
+    LANGUAGES = ["en", "fr"]
+    BABAL_DEFAULT_LOCALE = "en"
+    BABAL_DEFAULT_TIMEZONE = "UTC"
+# set the config class as the app's configuration
+
+
+app.config.from_object(Config)
+# Instantiate babel obj
+babel = Babel(app)
+
+
+# Define a route for the root URL ("/")
+@app.route("/")
+def index() -> str:
+    """this renders templates/0-index.html"""
+    return render_template("templates/0-index.html")
+
+
+if __name__ == "__main__":
+    """main lane runner"""
+    app.run(host="0.0.0.0", port=5000, debug=True)
