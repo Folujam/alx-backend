ae16de02ff9c656dd9183074008a6f1a54e770d1
folujam <olusway@gmail.com>
2024-07-31 20:05:23 +0100

    test t0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,11 +6,14 @@ from flask import Flask, render_template
 # Create a Flask app instance
 app = Flask(__name__)
 # Define a route for the root URL ("/")
+
+
 @app.route("/")
-def index():
+def index() -> str:
     """this renders templates/0-index.html"""
-    return render_template("templates/0-index.html")
+    return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     """main lane runner"""
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
